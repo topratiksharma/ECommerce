3c287aeb9e3a83c5a2c85984489327da717293a6
Pratik Sharma <pratiksharma@live.in>
2024-10-20 13:43:10 +0200

    Adding seed Data

---
+++ Services/Catalog/Catalog.Infrastructure/Data/BrandContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using CatalogAPI.Entities;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+
+namespace Catalog.Infrastructure.Data
+{
+    public class BrandContextSeed
+    {
+        public static void SeedData(IMongoCollection<ProductBrand> brandCollection)
+        {
+            bool existProductBrand = brandCollection.Find(p => true).Any();
+            string path = Path.Combine("Data", "SeedData", "brands.json");
+            if (!existProductBrand)
+            {
+                var brandsData = File.ReadAllText(path);
+                var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+                if (brands != null)
+                {
+                    foreach ( var brand in brands)
+                    {
+                        brandCollection.InsertOneAsync(brand);
+                    }
+                }
+            }
+        }
+    }
+}
